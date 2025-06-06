132903b6aec773a0a95b124d0dc0e0a71bc45fe5
Asok3217 <manandharasok@gmail.com>
2025-06-06 16:54:44 +0545

    nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './../assets/images/logo.png';
 import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv, HiPlus } from "react-icons/hi2";
 import HeaderItem from './HeaderItem'; // ✅ Importing the component
+import { HiDotsVertical } from 'react-icons/hi';
 
 const Header = () => {
   const menu = [
@@ -14,17 +15,29 @@ const Header = () => {
   ];
 
   return (
-    <div className=' bg-black flex justify-between p-5 '>
-        <div className='flex items-center gap-8'>
-      <img src={logo} alt="Disney Logo" className='w-[80px] md:w-[115px] object-cover mr-6' />
-      
-        {menu.map((item) => (
-          <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
-        ))}
+    <div className='bg-black flex justify-between p-5'>
+      <div className='flex items-center gap-8'>
+        <img src={logo} alt="Disney Logo" className='w-[80px] md:w-[115px] object-cover mr-6' />
+
+        {/* Mobile View (Only show 3 items and then the vertical dots icon) */}
+        <div className='flex md:hidden gap-8'>
+          {menu.slice(0, 3).map((item) => (
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+          ))}
+          <HeaderItem name={''} Icon={HiDotsVertical} />
+        </div>
+
+        {/* Desktop View (Show all menu items) */}
+        <div className='hidden md:flex gap-8'>
+          {menu.map((item) => (
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+          ))}
+        </div>
       </div>
+
       <img src="https://cdn.pixabay.com/photo/2020/06/30/10/23/icon-5355896_1280.png" alt="" className='w-[40px] rounded-full' />
     </div>
-  )
+  );
 }
 
 export default Header;
